62229be93078a64c0ad7cbc634a4314f64932db6
antaares <jrismoilov@gmail.com>
2023-04-10 14:55:23 +0500

    edit start

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,11 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
-from loader import dp
+from loader import dp, db
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}!")
+    if message.chat.type != "private":
+        db.add_group(message.chat.id)
